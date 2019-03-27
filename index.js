f957ebd514b51319c46218fa846dc1d70566b020
stephaniepinero <42078012+stephaniepinero@users.noreply.github.com>
2019-03-27 17:18:38 -0300

    Adding format Rut

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 'use strict';
 
 exports.validarRut = (rut) => {
-  //console.log("Validar rut", rut);
+ 
   let suma = 0;
-  rut = rut.replace(".", "").replace(".", "").replace(".", "").replace("-", "").replace(" ", "");
-  //console.log(rut);
+  rut = rut.replace(/\./g,"").replace(/\-/g,"");
+  
   let rutSolo = rut.slice(0, -1);
   let verif = rut.substr(rut.length - 1);
-
-  //console.log(rutSolo);
-  //console.log(verif);
   let continuar = true; 
   let i;
-  if(!Number.rutSolo){
+  if(!Number.rutSolo  && !parseInt(rutSolo)){
     return false;
   }
   if(rut.length > 9){
-    //console.log("Rut muy largo, es invalido ");
+
     return false;
   }
   for (i = 2; continuar; i++) {
@@ -27,20 +24,42 @@ exports.validarRut = (rut) => {
   }
   let resto = suma % 11;
   let dv = 11 - resto;
+
   if (dv == 10) {
     if (verif.toUpperCase() == 'K') {
-      //console.log("si");
       return true;
     }
   } else if (dv == 11 && verif == 0) {
-    //console.log("rut valido");
     return true;
   }
   else if (dv == verif) {
-    //console.log("rut valido");
     return true;
   } else {
-    //console.log("rut no valido");
     return false;
+  }
+}
+
+exports.formatRut = (rut, punto = false, guion = false) => {
+  rut = rut.replace(/\./g,"").replace(/\-/g,"")
+  if(!punto && !guion){
+    return rut;
+  }else{
+    let rutSolo = rut.slice(0, -1);
+    let verif = rut.substr(rut.length - 1);
+    let rutFinal = ""
+    if(punto){
+      rutSolo = rutSolo.toString().split('').reverse().join('').replace(/(?=\d*\.?)(\d{3})/g,'$1.');
+      rutSolo = rutSolo.split('').reverse().join('').replace(/^[\.]/,'');
+    }
+
+    rutFinal = rutSolo;
+
+    if(guion){
+      rutFinal += '-';
+    }
+
+    rutFinal += verif;
+    return rutFinal;
+
   }
 }
